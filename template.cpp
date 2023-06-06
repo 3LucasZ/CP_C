709f079bfb155d92d113fe38b9477d7698b06a90
3LucasZ <lucas.zheng@warriorlife.net>
2023-06-06 14:08:20 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <algorithm>
 #include <iterator>
 #include <vector>
@@ -8,10 +10,8 @@
 using namespace std;
 
 typedef long long ll;
-
 typedef pair<int, int> pi;
 typedef pair<ll,ll> pl;
-
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
@@ -24,16 +24,25 @@ template<class T> using pq = priority_queue<T>;
 #define lb lower_bound
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
-#define ins insert
+const char nl = '\n';
 
-const int MOD = 1000000007;
-bool multi = true;
-bool dbg = true;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+template<typename T, typename V>
+string toString(const pair<T, V> &x) {stringstream ret; ret << "{" << x.f << ", " << x.s << '}'; return ret.str();}
+template<typename T>
+string toString(const T &x) {int f = 0; stringstream ret; ret << "{"; for (auto &i: x) ret << (f++ ? ", " : "") << i; ret << "}"; return ret.str();}
+
+const bool multi = true;
+const bool dbg = true;
 
 void solve(){
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int T = 1;
     if (multi) cin >> T;
     for(int i=0;i<T;i++) {
