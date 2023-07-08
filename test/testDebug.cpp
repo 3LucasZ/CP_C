5cedafefb7206ab51b357f6e6b58f283a909c0e9
3LucasZ <lucas.zheng@warriorlife.net>
2023-07-08 16:52:16 -0700

    DEBUGGING THE TEMPLATE!

--- test/testDebug.cpp
+++ test/testDebug.cpp
@@ -1,21 +1,29 @@
-#include<iostream>
-#include<map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+typedef long long ll;
+typedef long double ld;
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+const char nl = '\n';
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
 void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(ld x) {cerr << x;}
 void __print(char x) {cerr << '\'' << x << '\'';}
 void __print(const char *x) {cerr << '\"' << x << '\"';}
 void __print(const string &x) {cerr << '\"' << x << '\"';}
 void __print(bool x) {cerr << (x ? "true" : "false");}
- 
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x);
 template<typename T, typename V>
 void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
 template<typename T>
@@ -32,13 +40,37 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define dbgM(x)
 #endif
 
+const bool multi = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main() {
-    map<int,int> mp;
-    mp[5]=3;
-    mp[9]++;
-    dbg(mp);
-    dbg(mp[3]);
-    dbg(mp[9]);
+    map<int,int> mp1;
+    mp1[1]=4;
+    dbg(mp1);
+
+    map<int,pair<int,int>> mp2;
+    mp2[1]={3,5};
+    dbg(mp2);
+    
+    vector<vector<vector<vector<int>>>> v1;
+    dbg(v1);
+
+    map<int, vector<int>> mp3;
+    vector<int> stuff = {3,4,5,5};
+    mp3[1]=stuff;
+    dbg(mp3);
+
     return 0;
 }
---
+++ test/testPairs.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    pair<int,int> stuff = {100,100};
+    //begin(stuff);
+    return 0;
+}
