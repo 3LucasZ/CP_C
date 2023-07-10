993b3082dfa7646d602eb63995e1f327acf2c4a4
3LucasZ <lucas.zheng@warriorlife.net>
2023-07-09 23:38:14 -0700

    Create genD.cpp

---
+++ src/genD.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+const char nl = '\n';
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void __print(int x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(ld x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#define dbgM(x) cout << "CASE: " << x << endl;
+#else
+#define dbg(x...)
+#define dbgM(x)
+#endif
+
+
+
+
+
+
+
+
+const int tcs = 1;
+
+template<class T> 
+T v_rand(vector<T> v){
+    T i = rand() % sz(v);
+    return v[i];
+}
+
+void tree(int l, int r) {
+    cout << r-l+1 << nl;
+    vector<int> pars = {l};
+    for (int u=l+1;u<=r;u++){
+        int v = v_rand(pars);
+        cout << u << " " << v << nl;
+        pars.push_back(u);
+    }
+}
+
+void gen(){
+    tree(0,10);
+}
+
+int main() {
+    srand(time(0));
+    if (tcs) {
+        cout << tcs << nl;
+        for (int i=0;i<tcs;i++) gen();
+    }
+    else {
+        gen();
+    }
+    return 0;
+}
