89e3d52eb0ce98d6e855dff3b9d18f2ef46cbb99
3LucasZ <lucas.zheng@warriorlife.net>
2023-07-24 19:36:17 -0700

    upd RURQ

--- STL/PURS/Lz_Seg_RURQ.cpp
+++ STL/PURS/Lz_Seg_RURQ.cpp
@@ -7,6 +7,7 @@ typedef long long ll;
 #define sz(x) (int)(x).size()
 #define all(x) x.begin(), x.end()
 const char nl = '\n';
+const ll inf = 2e18;
 
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
@@ -56,43 +57,44 @@ Settings:
 NEUTRAL, NO_OP, upd_op, qry_op, build(sets default val[]), DEF_OP(sets default op[])
 Extensions:
 (add, min)
-NEUTRAL = inf, NO_OP = 0, DEF_OP = 0, upd_op = a+b, qry_op = min(a,b)
+NEUTRAL = inf, upd_op = a+b, qry_op = min(a,b)
 (set, sum)
-NEUTRAL = 0, NO_OP = inf, DEF_OP = 0, upd_op = a | b*k, qry_op = a+b
+NEUTRAL = 0, upd_op = b*k, qry_op = a+b
 */
 
-ll MOD = 1e9+7;
+const ll MOD = 1e9+7;
 class SegTree {
     public:
-        ll NEUTRAL = 0; //no fx on qry
-        ll NO_OP = LLONG_MAX;  //label node as having no ops
-        ll DEF_OP = 0; //default op on each node during init
+        const ll NEUTRAL = inf; //no fx on qry
+        const ll DEF_VAL = 0; //default val on each node during init that has no build
         ll upd_op(ll a, ll b, ll k=1){ //plz ensure NO_OP has no fx on ops
-            if (b==NO_OP) return a;
-            return b*k;
-        }
-        ll qry_op(ll a, ll b){ 
+            if (a==NO_OP) return b; if (b==NO_OP) return a; //keep
             return a+b;
         }
-        void build(int x, int lx, int rx){ // build init val
-            if (lx==rx) val[x]=0;
+        ll qry_op(ll a, ll b){ 
+            return min(a,b);
+        }
+        void build(int x, int lx, int rx, const vector<int> &v){ // build init val
+            if (lx==rx) val[x]=lx-1<sz(v)?v[lx-1]:DEF_VAL;
             else {
                 int m = (lx+rx)/2;
-                build(2*x,lx,m);
-                build(2*x+1,m+1,rx);
+                build(2*x,lx,m,v);
+                build(2*x+1,m+1,rx,v);
                 val[x]=qry_op(val[2*x],val[2*x+1]);
+                ops[x]=NO_OP;
             }
         }
         //hidden------>
+        const ll NO_OP = inf+1;  //label node as having no ops
         int sz;
         vector<ll> ops; //lazy ops on node combined value
         vector<ll> val; //qry val of subtree
         SegTree(int n){
             sz = 1; while (sz < n) sz *= 2;
-            ops = vector<ll>(2*sz+1,DEF_OP);
+            ops = vector<ll>(2*sz+1,NO_OP);
             val = vector<ll>(2*sz+1,0);
-            build(1,1,sz);
         }
+        void build(const vector<int> &v = vector<int>()){build(1,1,sz,v);}
         void push_down(int x, int lx, int rx) { //maintain invariant top(new) - bot(old)
             if (lx==rx) return;
             ops[2*x]=upd_op(ops[2*x],ops[x]);
@@ -106,11 +108,11 @@ class SegTree {
             push_down(x,lx,rx);
             if (lx>r || rx<l) return; //seg out
             else if (l<=lx && rx<=r) { //seg full
-                ops[x]=upd_op(ops[x],v);
+                ops[x]=upd_op(ops[x],v); //can be sus: (NO_OP+v)
                 val[x]=upd_op(val[x],v,rx-lx+1);
             } else { //seg partial
                 int m = (lx+rx)/2;
-                update(l,r,v,2*x,lx,m); update(l,r,v,2*x+1,m+1,rx); 
+                update(l,r,v,2*x,lx,m); update(l,r,v,2*x+1,m+1,rx);
                 val[x]=qry_op(val[2*x],val[2*x+1]);
             }
         }
