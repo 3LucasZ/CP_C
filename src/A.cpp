2e910c8a65b00e3425348375bc5e1f6f5ff35623
3LucasZ <lucas.zheng@warriorlife.net>
2023-07-26 01:53:22 -0700

    yeet

---
+++ src/A.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+const char nl = '\n';
+const ll inf = 2e18;
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void __print(int x) {cerr << x;}
+void __print(unsigned int x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(ld x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '<'; __print(x.first); cerr << ", "; __print(x.second); cerr << '>';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#define dbgM(x) cout << "CASE: " << x << endl;
+#else
+#define dbg(x...)
+#define dbgM(x)
+#endif
+
+const bool multi = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve(){
+    int R, C, K; cin >> R >> C >> K;
+    int r1, r2; cin >> r1 >> r2;
+
+    bool caught = false;
+    for (int i=0;i<K;i++){
+        int u, v; cin >> u >> v;
+        if ((abs(u-r1)+abs(v-r2))%2==0) caught = true;
+    }
+
+    if (caught){
+        cout << "NO" << nl;
+    }
+    else {
+        cout << "YES" << nl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1; if (multi) cin >> T;
+    for(int i=0;i<T;i++) {dbgM(i+1);solve();}
+    return 0;
+}
---
+++ src/B.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+const char nl = '\n';
+const ll inf = 2e18;
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void __print(int x) {cerr << x;}
+void __print(unsigned int x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(ld x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '<'; __print(x.first); cerr << ", "; __print(x.second); cerr << '>';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#define dbgM(x) cout << "CASE: " << x << endl;
+#else
+#define dbg(x...)
+#define dbgM(x)
+#endif
+
+const bool multi = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve(){
+    int N, K; cin >> N >> K;
+    vector<int> A(N+2); for (int i=1;i<=N;i++) cin >> A[i];
+    dbg(N,K,A);
+
+    vector<vector<int>> B(N+1); 
+    for (int i=1;i<=N;i++) B[i].push_back(0);
+    for (int i=1;i<=N;i++){
+        B[A[i]].push_back(i);
+    }
+    for (int i=1;i<=N;i++) {B[i].push_back(N+1);B[i].push_back(N+2);}
+    dbg(B);
+
+    vector<vector<int>> gap(N+1);
+    for (int i=1;i<=N;i++){
+        for (int j=1;j<sz(B[i]);j++){
+            gap[i].push_back(B[i][j]-B[i][j-1]);
+        }
+    }
+    dbg(gap);
+
+    for (int i=1;i<=N;i++) sort(all(gap[i]),greater<int>());
+    dbg(gap);
+
+    int ans = inf;
+    for (int i=1;i<=N;i++){
+        ckmin(ans, max((gap[i][0]-1)/2,gap[i][1]-1));
+    }
+
+    cout << ans << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1; if (multi) cin >> T;
+    for(int i=0;i<T;i++) {dbgM(i+1);solve();}
+    return 0;
+}
