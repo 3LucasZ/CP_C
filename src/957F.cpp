83e4921e86aa5956de548c4bd5cf55ced24e47d2
3LucasZ <lucas.zheng@warriorlife.net>
2024-07-19 17:56:41 -0700

    Create 957F.cpp

---
+++ src/957F.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+const char nl = '\n';
+const ll inf = 2e18;
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void __print(int x) {cerr << x;}
+void __print(unsigned int x) {cerr << x;}
+void __print(ll x) {cerr << x;}
+void __print(ld x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '<'; __print(x.first); cerr << ", "; __print(x.second); cerr << '>';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#define dbgM(x) cout << "\e[34m" << "CASE: " << x << "\e[34m" << endl;
+#else
+#define dbg(x...)
+#define dbgM(x) 
+#endif
+
+const bool multi = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve(){
+    ll n,x;
+    cin>>n>>x;
+    vector<ll> a(n);
+    for (ll i=0;i<n;i++){
+        cin>>a[i];
+    }
+    dbg(n,x,a);
+
+    set<ll> cur{1};
+    ll ans = 1;
+    for (ll el:a){
+        //if element complements the set => create a new segment and cache the last one
+        if (x%el==0 && cur.count(x/el)==1){
+            cur.clear();
+            cur.insert(1);
+            ans++;
+        }
+        //"useful" element => update the set
+        if (x%el==0){
+            vector<ll> nxt;
+            for (ll s:cur){
+                if (x%(s*el)==0) {
+                    nxt.push_back(s*el);
+                }
+            }
+            for (ll nxtel:nxt){
+                cur.insert(nxtel);
+            }
+        }
+    }
+    cout << ans << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1; if (multi) cin >> T;
+    for(int i=0;i<T;i++) {dbgM(i+1);solve();}
+    return 0;
+}
